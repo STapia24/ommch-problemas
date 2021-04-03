a9ffabb436dea5f6928a93748b037f2d65ae89e6
ncgo <ngarciaorozco@outlook.com>
2021-04-03 01:37:32 -0600

    MERN Stack Tutorial Parte 2

---
+++ backend/problema.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Problema = new Schema({
+    problema_descripcion: {
+        type: String
+    },
+    problema_foto: {
+        type: String
+    },
+    problema_categoria: {
+        type: String
+    }, 
+    problema_agregado_por: {
+        type: String
+    },
+    problema_libro: {
+        type: String 
+    },
+    problema_nivel: {
+        type: String
+    },
+    problema_respuesta: {
+        type: String
+    }
+});
+
+module.expor = mongoose.model('Problema', Problema);
