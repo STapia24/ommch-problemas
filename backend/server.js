454e46ddf773d965746467d70702962294e375a7
ncgo <ngarciaorozco@outlook.com>
2021-04-03 00:47:44 -0600

    Backend files created

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 4000;
+
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/problemasOMMCH', { useNewUrlParser: true});
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successsfully");
+})
+
+app.listen(PORT, function() {
+    console.log("Server is running in Port: " + PORT);
+});
