99a416b9c99fe7e537fcf9b605c5685d23633f92
Sergio Tapia <tapia24@hotmail.com>
2021-05-25 00:36:39 -0700

    Added a route for the static build and scripts

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 const PORT = process.env.PORT || 4000 ;
 require('dotenv/config')
 
@@ -11,11 +12,20 @@ const getsRoute = require('./routes/gets');
 const postsRoute = require('./routes/posts');
 const deletesRoute = require('./routes/deletes');
 
+//Use Routes
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/gets', getsRoute);
 app.use('/posts', postsRoute);
 app.use('/deletes', deletesRoute);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
+    });
+}
 
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
